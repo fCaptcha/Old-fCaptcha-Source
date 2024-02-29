21fdafff0a0635ea019b8a1ec9c8439cf51a99de
DXVVAY <Arazrstmy500@gmail.com>
2024-02-29 21:22:22 +0100

    real asf

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import requests
 import time
 
@@ -28,9 +29,15 @@ class Captcha:
             except Exception as e:
                 print(f"Failed To Solve Captcha -> {e}")
 
-Captcha(
-    api_key="DEXV-ADMIN-D1IWNf-ZBMl8m-TutuyP",
-    sitekey='4c672d35-0701-42b2-88c3-78380b0db560',
-    url='discord.com', 
-    proxy="g5hltixerdytvio:g7w9oxoov0b8jsz@rp.proxyscrape.com:6060"
-).solve()
+def solve_captcha():
+    while True:
+        Captcha(
+            api_key="DEXV-ADMIN-D1IWNf-ZBMl8m-TutuyP",
+            sitekey='4c672d35-0701-42b2-88c3-78380b0db560',
+            url='discord.com', 
+            proxy="g5hltixerdytvio:g7w9oxoov0b8jsz@rp.proxyscrape.com:6060"
+        ).solve()
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    for _ in range(100):
+        executor.submit(solve_captcha)
