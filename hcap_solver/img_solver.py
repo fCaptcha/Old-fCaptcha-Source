8a72e17e06aa20d7960a4a0dec348d605ab666e1
DXVVAY <Arazrstmy500@gmail.com>
2024-04-19 21:44:00 +0200

    g

--- hcap_solver/img_solver.py
+++ hcap_solver/img_solver.py
@@ -17,27 +17,27 @@ version = re.search(r'v1/(.*?)/', js).group(1)
 
 hswwww = {
   "proof_spec": {
-    "difficulty": 2,
+    "difficulty": 16,
     "fingerprint_type": 0,
     "_type": "w",
-    "data": "KaGNA2b9ZmazONDC6sEAzWwWTFO2ZKQvxepbBnXLAuIyyyGBo9Zk03ESc0+0DQzo8JgoQjMxYyFibv6AmwSvC5YBuZHdcuj9GRv2TY+xTip7nbp9C4rUgZxL+srwnU2/fgBF4SlVDBSYcLdr+0RtFq0ejbA0JSc+gKpLiewa9A1/Qn5EdfAPkFDw99Q7WgeUCOmvoBtsYsoieBTFSG8TK05UWFiLM+9aTITKtsdkprg1NjVDxVWnOVbLGXfWiinV",
+    "data": "0B/+p0Vj9+Do93iyd/rbJSB4oDGXF4WdYHrs926ZOjTPsYpWXkMdRZz8qpA6j7PRZ6LsnncQzeSxjhGBWm026B644uani4GYX7E1SP7zliA2VFEqT5uzNq74rxV0xMbKdePjjU+/fTCFgLOyNbkk8sAROEng3CcZQs1q6scTl/bgI0YBcGp5GFdLMSgN17FnysNhCTHKhqJDHYioSouQ1v6AXMRSsk/UGHuHpVG+xrpNoP0B9Q==t8fuSBSiq2eB2ZEn",
     "_location": "https://newassets.hcaptcha.com/c/e78a38c",
     "timeout_value": 1000
   },
   "rand": [
-    0.5787595025663348,
-    0.02666437067091465
+    0.955140763884003,
+    0.7979516244959086
   ],
   "components": {
     "navigator": {
-      "user_agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.221 Chrome/120.0.6099.291 Electron/28.2.10 Safari/537.36",
-      "language": "en-US",
+      "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+      "language": "en-AU",
       "languages": [
-        "en-US",
-        "en-SE",
-        "en-GB",
+        "en-AU",
         "fa",
-        "sv-SE"
+        "en-US",
+        "en",
+        "sv"
       ],
       "platform": "Win32",
       "max_touch_points": 0,
@@ -60,8 +60,8 @@ hswwww = {
     "web_gl_hash": "-1",
     "canvas_hash": "3290922276056775431",
     "has_touch": False,
-    "notification_api_permission": "Granted",
-    "chrome": False,
+    "notification_api_permission": "Denied",
+    "chrome": True,
     "to_string_length": 33,
     "err_firefox": None,
     "r_bot_score": 0,
@@ -71,13 +71,13 @@ hswwww = {
     "extensions": [
       False
     ],
-    "parent_win_hash": "18230616880885635051",
+    "parent_win_hash": "5683397452819783432",
     "webrtc_hash": "-1",
-    "performance_hash": "4140103483592612201",
-    "unique_keys": "__localeData__,DiscordNative,regeneratorRuntime,2,0,__BILLING_STANDALONE__,webpackChunkdiscord_app,platform,__SECRET_EMOTION__,__SENTRY__,hcaptcha,__SENTRY_IPC__,hcaptchaOnLoad,__timingFunction,DiscordErrors,clearImmediate,__OVERLAY__,grecaptcha,DiscordSentry,GLOBAL_ENV,setImmediate,1,IntlPolyfill,createDiscordStream,_ws,popupBridge,__DISCORD_WINDOW_ID",
-    "inv_unique_keys": "__wdata,sessionStorage,localStorage,hsw,_sharedLibs",
-    "common_keys_hash": 276567530,
-    "common_keys_tail": "chrome,caches,cookieStore,ondevicemotion,ondeviceorientation,ondeviceorientationabsolute,launchQueue,documentPictureInPicture,onbeforematch,getScreenDetails,openDatabase,queryLocalFonts,showDirectoryPicker,showOpenFilePicker,showSaveFilePicker,originAgentCluster,credentialless,speechSynthesis,oncontentvisibilityautostatechange,onscrollend,webkitRequestFileSystem,webkitResolveLocalFileSystemURL,Raven",
+    "performance_hash": "5480068218625584110",
+    "unique_keys": "1,onSuccess,hcaptcha,0,CMExtension,onExpire,grecaptcha",
+    "inv_unique_keys": "hsw,__wdata,exportsInstance,image_label_binary,image_label_area_select,_sharedLibs",
+    "common_keys_hash": 2005040245,
+    "common_keys_tail": "stop,structuredClone,webkitCancelAnimationFrame,webkitRequestAnimationFrame,chrome,fence,caches,cookieStore,ondevicemotion,ondeviceorientation,ondeviceorientationabsolute,launchQueue,documentPictureInPicture,getScreenDetails,queryLocalFonts,showDirectoryPicker,showOpenFilePicker,showSaveFilePicker,originAgentCluster,onpagereveal,credentialless,speechSynthesis,onscrollend,webkitRequestFileSystem,webkitResolveLocalFileSystemURL,define,ethereum,ClownfishModuleInit,CF_Settings,createScriptProcessorHooked,StackTrace,cetus,Raven",
     "features": {
       "performance_entries": True,
       "web_audio": True,
@@ -88,156 +88,76 @@ hswwww = {
   },
   "events": [
     [
-      1340556786,
-      "true"
-    ],
-    [
-      2258007971,
-      "4932383211497360507"
-    ],
-    [
-      4286617223,
-      "[1920,1080,1920,1040,24,24,false,0,1,1920,1040,true,true,true,false]"
-    ],
-    [
-      3737842855,
-      "1117"
-    ],
-    [
-      2571423665,
-      "86.60000002384186"
-    ],
-    [
-      1578658063,
-      "[1,2,3,4]"
-    ],
-    [
-      157020890,
-      "17002384262467705698"
-    ],
-    [
-      364527618,
-      "[19]"
-    ],
-    [
-      3369879498,
-      "11038406483972230190"
-    ],
-    [
-      1845702994,
-      "15307345790125003576"
-    ],
-    [
-      645217632,
-      "[1,1024,1,1,4]"
-    ],
-    [
-      1885396794,
-      "[16384,32,16384,2048,2,2048]"
-    ],
-    [
-      1767811539,
-      "4226317358175830201"
-    ],
-    [
-      422388259,
-      "[[[\"https://newassets.hcaptcha.com/captcha/v1/b1c589a/hcaptcha.js\",0,5]],[[\"*\",84,9]]]"
-    ],
-    [
-      3652805059,
-      "[4,120,4]"
-    ],
-    [
-      3444878050,
-      "[[277114314453,277114314460,277114314451,357114314456,277114314452,554228628898,57114314443,717114314371391,554228628897,277114314456,1108457257862,277114314450,554228628919,277114314460,277114314451],false]"
-    ],
-    [
-      702607242,
-      "1713548957202"
-    ],
-    [
-      3260504850,
-      "[[193,[193,193,193,255,193,193,193,255,193,193,193,255,193,193,193,255]],[[11,0,1,105.015625,13,5,105.6171875],[[12,0,-1,113.125,17,4,113],[11,0,0,111,12,4,111],[11,0,1,105.015625,13,5,105.6171875],[11,0,1,105.015625,13,5,105.6171875],[11,0,1,105.015625,13,5,105.6171875],[11,0,1,105.015625,13,5,105.6171875],[11,0,1,105.015625,13,5,105.6171875],[11,0,1,105.015625,13,5,105.6171875],[12,0,0,109.640625,14,3,110.1953125]]],[0,2,4,5,6,8,9,12,13,15,17,18,19,21,22,23,28,29,30,31,33,34,35,37,39,42,47,48,49,66,67,69,71,72,75,76,77,78,79,81,82],[0,0,0,0,14,3,0]]"
-    ],
-    [
-      1930240485,
-      "[26046259200,26046259200,null,null,1098907648,true,true,true,null]"
-    ],
-    [
-      1392191255,
-      "[\"TGbeRxCU9GzGJtJSNnbiP2zU\",\"19\",\"15\",\"PCZEALPHTWULX\"]"
-    ],
-    [
-      1192669439,
-      "13177607191192652685"
-    ],
-    [
-      1476007714,
-      "[[true,\"en-US\",true,\"Microsoft David - English (United States)\",\"Microsoft David - English (United States)\"],[false,\"en-GB\",true,\"Microsoft Hazel - English (United Kingdom)\",\"Microsoft Hazel - English (United Kingdom)\"],[false,\"en-GB\",true,\"Microsoft Susan - English (United Kingdom)\",\"Microsoft Susan - English (United Kingdom)\"]]"
-    ],
-    [
-      980782277,
-      "[32767,32767,16384,8,8,8]"
-    ],
-    [
-      355545911,
-      "14882824317255702596"
-    ],
-    [
-      206443282,
-      "3080.7000000476837"
-    ],
-    [
-      3635805594,
-      "[\"5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.221 Chrome/120.0.6099.291 Electron/28.2.10 Safari/537.36\",\"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.221 Chrome/120.0.6099.291 Electron/28.2.10 Safari/537.36\",8,4,\"en-US\",[\"en-US\",\"en-SE\",\"en-GB\",\"fa\",\"sv-SE\"],\"Win32\",null,[\"Not_A Brand 8\",\"Chromium 120\"],false,\"Windows\",2,5,true,false,50,false,false,false,\"[object Keyboard]\",false,false]"
-    ],
-    [
-      1196101075,
-      "[16,1024,4096,7,12,120,[23,127,127]]"
-    ],
-    [
       3741287486,
       "[2147483647,2147483647,4294967294]"
-    ],
-    [
-      40999691,
-      "[\"NWQJNyUtOyADM=IJ\",\"11\",\"3\",\"CZKRKJCGGTWWW\"]"
-    ],
-    [
-      2264178394,
-      "\"Europe/Stockholm\""
     ],
     [
       1578500414,
       "16153807394097295248"
     ],
     [
-      3961532981,
-      "[-6.172840118408203,-20.710678100585938,120.71067810058594,-20.710678100585938,141.42135620117188,120.71067810058594,-20.710678100585938,141.42135620117188,-20.710678100585938,-20.710678100585938,0,0,300,150,false]"
+      3635805594,
+      "[\"5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\",\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36\",8,4,\"en-AU\",[\"en-AU\",\"fa\",\"en-US\",\"en\",\"sv\"],\"Win32\",null,[\"Google Chrome 123\",\"Not:A-Brand 8\",\"Chromium 123\"],false,\"Windows\",2,5,true,false,50,false,false,true,\"[object Keyboard]\",false,false]"
     ],
     [
-      956283155,
-      "[\"Windows\",\"10.0.0\",null,\"64\",\"x86\",\"120.0.6099.291\"]"
+      422388259,
+      "[[[\"https://newassets.hcaptcha.com/captcha/v1/b1c589a/hcaptcha.js\",0,5]],[[\"*\",84,9],[null,92,1],[null,92,1],[null,92,1],[null,92,1],[null,92,1],[null,92,1],[null,92,1],[null,92,1],[null,92,1]]]"
     ],
     [
-      4174694926,
-      "[[],35,34,null,false,false,true,37,true,true,true,true,true,[\"Raven\",\"_sharedLibs\",\"hsw\",\"__wdata\"],[[\"getElementsByClassName\",[]],[\"getElementById\",[]],[\"querySelector\",[]],[\"querySelectorAll\",[]]],[],true]"
+      1192669439,
+      "13177607191192652685"
     ],
     [
-      3157273169,
-      "56.799999952316284"
+      1438921646,
+      "[[\"css:imgs3.hcaptcha.com\",0,34.299999952316284],[\"img:imgs3.hcaptcha.com\",0,122.64999997615814],[\"navigation:newassets.hcaptcha.com\",101,137.80000007152557],[\"script:newassets.hcaptcha.com\",4,16.849999964237213],[\"xmlhttprequest:api.hcaptcha.com\",0,305.10000002384186],[\"xmlhttprequest:api2.hcaptcha.com\",0,205.70000004768372]]"
+    ],
+    [
+      1116177314,
+      "855.7999999523163"
+    ],
+    [
+      40999691,
+      "[\"5ETO4UdOyKTO2idN\",\"1a\",\"8\",\"XHAVSCJDILKDL\"]"
+    ],
+    [
+      1833974975,
+      "11162887117610606218"
+    ],
+    [
+      1933294324,
+      "[16,4095,30,16,16380,120,12,120,[23,127,127]]"
+    ],
+    [
+      2638668498,
+      "[24,24,65536,212988,200704]"
+    ],
+    [
+      3737842855,
+      "1134"
     ],
     [
       689964981,
       "57"
     ],
     [
-      2946108189,
-      "[0,18407,18407]"
+      157020890,
+      "12027682292028963860"
     ],
     [
-      1438921646,
-      "[[\"navigation:newassets.hcaptcha.com\",56.799999952316284,75.10000002384186],[\"script:newassets.hcaptcha.com\",36.35000002384186,229.69999998807907],[\"xmlhttprequest:api.hcaptcha.com\",0,182]]"
+      2338842265,
+      "3313549113868922289"
+    ],
+    [
+      202322096,
+      "2337666753322697468"
+    ],
+    [
+      980782277,
+      "[32767,32767,16384,8,8,8]"
+    ],
+    [
+      4174694926,
+      "[[\"loadTimes\",\"csi\",\"app\"],35,34,null,false,false,true,37,true,true,true,true,true,[\"define\",\"ethereum\",\"ClownfishModuleInit\",\"CF_Settings\",\"createScriptProcessorHooked\",\"StackTrace\",\"cetus\",\"Raven\",\"_sharedLibs\",\"hsw\",\"exportsInstance\",\"__wdata\",\"image_label_binary\",\"image_label_area_select\",\"web3\"],[[\"getElementsByClassName\",[]],[\"getElementById\",[]],[\"querySelector\",[]],[\"querySelectorAll\",[]]],[],true]"
     ],
     [
       3298983941,
@@ -245,66 +165,169 @@ hswwww = {
     ],
     [
       2883186606,
-      "[[\"lQUlCmYUiQNZNLwITjahga6VgeOc0TCD\",\"13\",\"6\",\"MMSDYPGJKXNDO\"],[\"zRMZwITJPDLSfpKqwITJo80VkVKSCNKmLajmLI0TI1KRfNTRxETkwITJxR3X18fmdJTJwITJ3R3X18fmwITJfp2cMRWDzuumxajmLGCm5bdmwaTmhHjmpajmLEjN1admyuCSVLfmyuCSMDEc\",\"1\",\"64\",\"DAMHMQFBLBGUK\"]]"
+      "[[\"xmLWiTJQHheWvNBOCvRS1KuklcMyuIu3\",\"9\",\"11\",\"UIHCDNVBOZKWC\"],[\"VMxAJMlGcMzBdMWATMBhJMPAJMlEJN1AdMyuIQpNFMyuIQ6FkathneWiTjjFFrZrkVWiTjOkuuElFrwNkMlAJMlYVScDkwZNTwxETKWiTjR52X18FMdjTjWiTjx52X18FMWiTjZrwb6h3bzk\",\"15\",\"32\",\"OIPDJCGGWUDRD\"]]"
+    ],
+    [
+      1578658063,
+      "[1,2,3,4]"
+    ],
+    [
+      1392191255,
+      "[\"fmyUP2ZKvGDGJTJIlGZYP3DK\",\"f\",\"15\",\"WYPVHCFSKPDVN\"]"
+    ],
+    [
+      645217632,
+      "[1,1024,1,1,4]"
+    ],
+    [
+      364527618,
+      "[21]"
+    ],
+    [
+      781302243,
+      "35257"
+    ],
+    [
+      4286617223,
+      "[1920,1080,1920,1040,24,24,false,0,1,1920,1040,true,true,true,false]"
+    ],
+    [
+      3961532981,
+      "[-6.172840118408203,-20.710678100585938,120.71067810058594,-20.710678100585938,141.42135620117188,120.71067810058594,-20.710678100585938,141.42135620117188,-20.710678100585938,-20.710678100585938,0,0,400,600,false]"
+    ],
+    [
+      355545911,
+      "165624761953926771"
+    ],
+    [
+      1845702994,
+      "15307345790125003576"
+    ],
+    [
+      1885396794,
+      "[16384,32,16384,2048,2,2048]"
+    ],
+    [
+      1340556786,
+      "true"
+    ],
+    [
+      702607242,
+      "1713551954614.5"
+    ],
+    [
+      3369879498,
+      "18207788058829391080"
+    ],
+    [
+      2258007971,
+      "4932383211497360507"
+    ],
+    [
+      1476007714,
+      "[[true,\"en-US\",true,\"Microsoft David - English (United States)\",\"Microsoft David - English (United States)\"],[false,\"en-GB\",true,\"Microsoft Hazel - English (United Kingdom)\",\"Microsoft Hazel - English (United Kingdom)\"],[false,\"en-GB\",true,\"Microsoft Susan - English (United Kingdom)\",\"Microsoft Susan - English (United Kingdom)\"]]"
+    ],
+    [
+      3157273169,
+      "4.5"
+    ],
+    [
+      2571423665,
+      "94.29999995231628"
+    ],
+    [
+      2264178394,
+      "\"Europe/Stockholm\""
+    ],
+    [
+      1767811539,
+      "7245447288404687584"
     ],
     [
       612273951,
       "[\"Europe/Stockholm\",-60,-60,-3203646808000,\"Central European Standard Time\",\"en-US\"]"
     ],
     [
-      202322096,
-      "2337666753322697468"
+      2946108189,
+      "[0,10465,10465]"
     ],
     [
-      106767486,
-      "[\"Google Inc. (NVIDIA)\",\"ANGLE (NVIDIA, NVIDIA GeForce GTX 1650 (0x00001F82) Direct3D11 vs_5_0 ps_5_0, D3D11)\"]"
+      3504714987,
+      "[1,4,5,7,9,12,20,21,24,25,29,31]"
+    ],
+    [
+      3260504850,
+      "[[196,[196,196,196,255,196,196,196,255,196,196,196,255,196,196,196,255]],[[11,0,0,95.96875,15,4,96.765625],[[12,0,-1,113.125,17,4,113],[11,0,0,111,12,4,111],[11,0,0,95.96875,15,4,96.765625],[11,0,0,95.96875,15,4,96.765625],[11,0,0,95.96875,15,4,96.765625],[11,0,0,95.96875,15,4,96.765625],[11,0,0,95.96875,15,4,96.765625],[11,0,0,95.96875,15,4,96.765625],[12,0,0,109.640625,14,3,110.1953125]]],[0,2,4,5,6,8,9,12,13,15,17,18,19,21,22,23,28,29,30,31,33,34,35,37,39,42,47,48,49,66,67,69,71,72,75,76,77,78,79,81,82],[0,0,0,0,14,3,0]]"
+    ],
+    [
+      1930240485,
+      "[299624919858,299624919858,null,null,4294705152,true,true,true,null]"
+    ],
+    [
+      2731828028,
+      "1526195183414568207"
+    ],
+    [
+      3672350690,
+      "125.80000007152557"
+    ],
+    [
+      3652805059,
+      "[4,120,4]"
     ],
     [
       3646847539,
       "[2147483647,2147483647,2147483647,2147483647]"
     ],
     [
-      1933294324,
-      "[16,4095,30,16,16380,120,12,120,[23,127,127]]"
+      1196101075,
+      "[16,1024,4096,7,12,120,[23,127,127]]"
+    ],
+    [
+      3444878050,
+      "[[277114314453,277114314460,277114314451,357114314456,277114314452,554228628898,57114314443,717114314371391,554228628897,277114314456,1108457257863,277114314450,554228628919,277114314460,277114314451],false]"
+    ],
+    [
+      206443282,
+      "3616525.100000024"
     ],
     [
       3004217393,
-      "627"
-    ],
-    [
-      2638668498,
-      "[24,24,65536,212988,200704]"
+      "631"
     ],
     [
       2122633777,
-      "12073207331849695208"
+      "10709114373335899612"
     ],
     [
-      2338842265,
-      "3313549113868922289"
+      956283155,
+      "[\"Windows\",\"10.0.0\",null,\"64\",\"x86\",\"123.0.6312.122\"]"
     ],
     [
-      1833974975,
-      "16290568259171983358"
-    ],
-    [
-      3672350690,
-      "137.75"
-    ],
-    [
-      2731828028,
-      "4631229088072584217"
-    ],
-    [
-      3504714987,
-      "[1,4,5,7,9,12,20,21,24,25,29,31]"
+      106767486,
+      "[\"Google Inc. (NVIDIA)\",\"ANGLE (NVIDIA, NVIDIA GeForce GTX 1650 (0x00001F82) Direct3D11 vs_5_0 ps_5_0, D3D11)\"]"
     ]
   ],
   "suspicious_events": [],
-  "messages": None,
-  "stack_data": None,
-  "stamp": "1:2:2024-04-19:KaGNA2b9ZmazONDC6sEAzWwWTFO2ZKQvxepbBnXLAuIyyyGBo9Zk03ESc0+0DQzo8JgoQjMxYyFibv6AmwSvC5YBuZHdcuj9GRv2TY+xTip7nbp9C4rUgZxL+srwnU2/fgBF4SlVDBSYcLdr+0RtFq0ejbA0JSc+gKpLiewa9A1/Qn5EdfAPkFDw99Q7WgeUCOmvoBtsYsoieBTFSG8TK05UWFiLM+9aTITKtsdkprg1NjVDxVWnOVbLGXfWiinV::ph4uKgxE:5",
-  "href": "https://canary.discord.com/register?redirect_to=%2Fchannels%2F%40me%2F1230637003364630639",
+  "messages": [
+    [
+      "https://newassets.hcaptcha.com",
+      [
+        "{\"target\":\"metamask-contentscrip",
+        "{\"target\":\"metamask-inpage\",\"dat",
+        "{\"target\":\"metamask-inpage\",\"dat",
+        "{\"target\":\"metamask-inpage\",\"dat",
+        "{\"target\":\"metamask-contentscrip",
+        "{\"target\":\"metamask-inpage\",\"dat"
+      ]
+    ]
+  ],
+  "stack_data": [
+    "r.sendSiteMetadata (<anonymous>)\ne (<anonymous>)",
+    "new Promise (<anonymous>)"
+  ],
+  "stamp": "1:16:2024-04-19:0B/+p0Vj9+Do93iyd/rbJSB4oDGXF4WdYHrs926ZOjTPsYpWXkMdRZz8qpA6j7PRZ6LsnncQzeSxjhGBWm026B644uani4GYX7E1SP7zliA2VFEqT5uzNq74rxV0xMbKdePjjU+/fTCFgLOyNbkk8sAROEng3CcZQs1q6scTl/bgI0YBcGp5GFdLMSgN17FnysNhCTHKhqJDHYioSouQ1v6AXMRSsk/UGHuHpVG+xrpNoP0B9Q==t8fuSBSiq2eB2ZEn::A5OfBhXt:89b9",
+  "href": "https://accounts.hcaptcha.com/demo",
   "ardata": None,
   "errs": {
     "list": []
@@ -312,15 +335,11 @@ hswwww = {
   "perf": [
     [
       1,
-      25
+      20
     ],
     [
       2,
-      98
-    ],
-    [
-      3,
-      0
+      868
     ]
   ]
 }
