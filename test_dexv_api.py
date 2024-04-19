f5546bfd49c2e8cd681caf438b6318b1ae6e28dc
DXVVAY <Arazrstmy500@gmail.com>
2024-04-19 21:32:57 +0200

    fixed

--- test_dexv_api.py
+++ test_dexv_api.py
@@ -38,7 +38,7 @@ def solve_captcha():
             api_key="DEXV-ADMIN-71BczP-nssbPD-eR61cH",
             sitekey='4c672d35-0701-42b2-88c3-78380b0db560',
             url='discord.com',
-            proxy="theproproxy:uXJwhcxE@geo.iproyal.com:12321"
+            proxy="766aptlkzt5xvy9:he41d5uvjuc2udc@rp.proxyscrape.com:6060"
         ).solve()
 
 
--- test_local_api.py
+++ test_local_api.py
@@ -4,5 +4,5 @@ from hcap_solver import Hcaptcha
 Hcaptcha(
     site_key='4c672d35-0701-42b2-88c3-78380b0db560',
     host='discord.com', 
-    proxy="theproproxy:uXJwhcxE@geo.iproyal.com:12321"
+    proxy="766aptlkzt5xvy9:he41d5uvjuc2udc@rp.proxyscrape.com:6060"
 ).solve()
