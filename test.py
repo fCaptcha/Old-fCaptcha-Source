bb5a97fb76d48be4c992864fd82280b0be22bca8
dort. <122418336+discord-trollering@users.noreply.github.com>
2024-04-23 18:53:14 -0400

    bro :skull:

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-from hcap_solver import *
+import multiprocessing
 
-g =Hcaptcha(
-    site_key="4c672d35-0701-42b2-88c3-78380b0db560",
-    host="discord.com",
-    proxy=f"user-10014101_25-country-US-session-636321:Mt0mdR7aLHOL5@geo.iproyal.vip:8888",
-).solve()
+from tools.DiscordGen import proc
 
-print(g)
+if __name__ == '__main__':
+    for i in range(2):
+        multiprocessing.Process(target=proc).start()
