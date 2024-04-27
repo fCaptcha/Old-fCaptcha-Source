8d6569b5d58596f9ca223a33cff2d5c9945c2a06
DXVVAY <Arazrstmy500@gmail.com>
2024-04-27 21:05:59 +0200

    there

--- api.py
+++ api.py
@@ -8,21 +8,17 @@ import string
 
 app = Flask(__name__)
 
-# Connect to MongoDB
 client = MongoClient('mongodb+srv://dev:hwOhSJASFYGKx8sb@cluster0.dgh1m9c.mongodb.net/')
 db = client['fcaptcha']
 collection = db['users']
 
-# keep this empty cus pro db shit
 task_status = {}
 
 admin_key = "fuckjews123"
-def generate_api_key():
-    apikey = 'fcap-' + '-'.join([''.join(random.choices(string.ascii_lowercase + string.digits, k=4)) for _ in range(3)])
-    return apikey
+def generate_api_key() -> str:
+    return f"fcap-{'-'.join([''.join(random.choices(string.ascii_lowercase + string.digits, k=4)) for _ in range(3)])}"
 
-
-def solve_captcha_task(api_key, task_id, sitekey, host, proxy, rqdata=None, useragent=None):
+def solve_captcha_task(api_key: str, task_id: str, sitekey: str, host: str, proxy: str, rqdata: str = None, useragent: str = None) -> None:
     start = time.time()
     if sitekey == "4c672d35-0701-42b2-88c3-78380b0db560":
         cost = 0.0005
@@ -38,7 +34,7 @@ def solve_captcha_task(api_key, task_id, sitekey, host, proxy, rqdata=None, user
     ).solve()
     if captcha_key is None:
         task_status[task_id] = {
-            "error": False,
+            "error": True,
             "task": {
                 "captcha_key": captcha_key,
                 "time": time.time() - start,
@@ -54,7 +50,7 @@ def solve_captcha_task(api_key, task_id, sitekey, host, proxy, rqdata=None, user
                 "state": "completed",
             },
         }
-        deduct = collection.find_one_and_update(
+        collection.find_one_and_update(
             {"api_key": api_key},
             {"$inc": {"balance": -cost}}
         )
@@ -86,7 +82,6 @@ def create_api_key():
         'api_key': apikey
     }), 201
 
-
 @app.route('/admin/delete', methods=['DELETE'])
 def remove_api_key():
     api_key = request.headers.get('authorization')
@@ -106,6 +101,35 @@ def remove_api_key():
     return jsonify({
         'message': 'Deleted'
     })
+
+@app.route('/admin/update_key', methods=['POST'])
+def update_api_key_balance():
+    api_key = request.headers.get('authorization')
+    if not api_key:
+        return jsonify({"error": True, "message": "API key is missing"}), 401
+    user = admin_key
+    if not user:
+        return jsonify({"error": True, "message": "Invalid API key"}), 401
+
+    data = request.json
+    if "balance" not in data or data["balance"] <= 0:
+        return jsonify({
+            'message': 'Invalid balance'
+        }), 400
+
+    result = collection.update_one(
+        {'api_key': api_key},
+        {'$set': {'balance': data["balance"]}}
+    )
+
+    if result.matched_count == 0:
+        return jsonify({
+            'message': 'API key not found'
+        }), 404
+
+    return jsonify({
+        'message': 'Balance updated successfully'
+    }), 200
 
 # Solving
 @app.route("/solve", methods=["POST"])
@@ -139,17 +163,14 @@ def solve_captcha_route():
 
     logger.log.info(f"New Task Created: {task_id} From IP: {ip_address}")
 
-    if rq_data or user_agent is not None:
-        threading.Thread(
-            target=solve_captcha_task, args=(api_key, task_id, sitekey, host, proxy, rq_data, user_agent)
-        ).start()
-    else:
-        threading.Thread(
-            target=solve_captcha_task, args=(api_key, task_id, sitekey, host, proxy)
-        ).start()
+    rq_data = rq_data if rq_data != "" else None
+    user_agent = user_agent if user_agent != "" else None
+
+    threading.Thread(
+        target=solve_captcha_task, args=(api_key, task_id, sitekey, host, proxy, rq_data, user_agent)
+    ).start()
 
     return jsonify({"error": False, "task": {"task_id": task_id, "state": "processing"}})
-
 
 @app.route("/getTaskData/<task_id>", methods=["GET"])
 def get_status(task_id):
@@ -166,6 +187,25 @@ def get_status(task_id):
 
     return jsonify(task_status[task_id])
 
+@app.route('/get_balance/<api_key>', methods=['GET'])
+def get_api_key_balance(api_key):
+    if not api_key:
+        return jsonify({"error": True, "message": "API key is missing"}), 401
+    user = admin_key
+    if not user:
+        return jsonify({"error": True, "message": "Invalid API key"}), 401
+
+    result = collection.find_one({'api_key': api_key})
+
+    if not result:
+        return jsonify({
+            'message': 'API key not found'
+        }), 404
+
+    return jsonify({
+        'message': 'Balance retrieved successfully',
+        'balance': result['balance']
+    }), 200
 
 if __name__ == '__main__':
     app.run(
