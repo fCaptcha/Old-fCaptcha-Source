5a54276e0c7b507a78013854c720b834431de3fd
DXVVAY <Arazrstmy500@gmail.com>
2024-05-13 00:52:13 +0200

    trying this ig

--- fcaptcha/hsw.py
+++ fcaptcha/hsw.py
@@ -1,8 +1,10 @@
 import base64
 import json
+import secrets
 import time
 import httpx
 import tls_client
+import xxhash
 from redis import Redis
 import random
 import hashlib
@@ -12,7 +14,10 @@ from random import randint as r
 from time import strftime, localtime
 
 client = tls_client.Session("chrome_104", random_tls_extension_order=True)
-database_fps = Redis("45.45.238.213", 42081, 313, "ACCA5B570561DCFA5ACB1417C69F2900DAFF8A4FD39A2E66C36DF2BD796F0BE1CFEA8AF2DB18153874215E08BFDEC4A89A397EC53E52DAC33A1E9D0B17A52D43")
+database_fps = Redis("45.45.238.213", 42081, 313,
+                     "ACCA5B570561DCFA5ACB1417C69F2900DAFF8A4FD39A2E66C36DF2BD796F0BE1CFEA8AF2DB18153874215E08BFDEC4A89A397EC53E52DAC33A1E9D0B17A52D43")
+
+hash_json = lambda x: str(xxhash.xxh64(json.dumps(x, separators=(",", ":")), seed=5575352424011909552).intdigest())
 
 
 class HSW:
@@ -34,45 +39,55 @@ class HSW:
     def random_float(self) -> float:
         return random.uniform(0.0000000000000001, 0.9999999999999999)
 
-    def pull(self, jwt: str, host: str, user_agent: str) -> str:
+    def pull(self, jwt: str, host: str, user_agent: str) -> str | None:
         s = jwt.split(".")[1].encode()
         s += b'=' * (-len(s) % 4)
         parsed = json.loads(base64.b64decode(s, validate=False).decode())
         hc_diff = parsed['s']
         hc_data = parsed['d']
-        if data := json.load(open("hsw_types/discord.json", "r")):
+        if data := json.load(open("hsw_types/discord.json")):
             data["stamp"] = self.mint(hc_data, hc_diff)
-            data["components"]["canvas_hash"] = str(random.randint(1000000000000000000,9999999999999999999))
-            data["components"]["parent_win_hash"] = str(random.randint(10000000000000000000,99999999999999999999))
-            data["components"]["performance_hash"] = str(random.randint(10000000000000000000, 99999999999999999999))
-            data["components"]["common_keys_hash"] = random.randint(1000000000,9999999999)
             data["rand"] = [self.random_float(), self.random_float()]
             data["href"] = f"https://{host}"
             data["proof_spec"]["data"] = hc_data
             data["proof_spec"]["difficulty"] = hc_diff
             data["stack_data"] = ["new Promise (<anonymous>)"]
-            data["perf"][0][1] = round(random.uniform(10, 20), 0)
-            data["perf"][1][1] = round(random.uniform(84, 99), 0)
-            data["perf"][2][1] = round(random.uniform(0, 1), 0)
             random.shuffle(data["events"])
+            rand = random.randint(1, 254)
+            ev = [rand,
+                  [rand + 1, rand, rand, 255, rand + 1, rand + 1, rand, 255, rand + 1, rand, rand, 255, rand + 1, rand,
+                   rand + 1, 255]]
+            hashed = hash_json(ev)
             for event in data["events"]:
                 match event[0]:
+                    case 3498175614:
+                        event[1] = json.dumps(f"[\"{base64.b64encode(secrets.token_bytes(16)).decode()}\"]")
+                    case 3916977893:
+                        event[1] = json.dumps(f"[\"{base64.b64encode(secrets.token_bytes(16)).decode()}\"]")
+                    case 443486963:
+                        event[1] = hashed
+                    case 143592240:
+                        var0 = json.loads(event[1])
+                        var0[0] = ev
+                        event[1] = json.dumps(var0, separators=(",", ":"))
                     case 1587180961:
-                        event[1] = str(round(time.time() * 1000, 1))
+                        event[1] = str(int(time.time() * 1000))
             hsw_str = self.encrypt(json.dumps(data, separators=(",", ":")))
             return hsw_str
         return None
 
     def mint(self, resource: str, bits: int = 2, ext: str = '', salt_chars: int = 8) -> str:
         timestamp = strftime("%Y-%m-%d", localtime(time.time()))
-        challenge = f"1:{bits}:{timestamp}:{resource}:{ext}:{self.get_salt(salt_chars)}"
+        challenge = f"1:{bits}:{timestamp}:{resource}:{ext}:{self.get_salt(salt_chars)}:"
         return f"{challenge}{self.mint_stamp(challenge, bits)}"
 
-    def get_salt(self, data_in: int) -> str:
+    @staticmethod
+    def get_salt(data_in: int) -> str:
         charset = ascii_letters + "+/="
         return ''.join([random.choice(charset) for _ in [None] * data_in])
 
-    def mint_stamp(self, challenge: str, bits: int) -> str:
+    @staticmethod
+    def mint_stamp(challenge: str, bits: int) -> str:
         counter = 0
         hex_digits = int(ceil(bits / 4.0))
         zeros = '0' * hex_digits
--- test_local_api.py
+++ test_local_api.py
@@ -5,7 +5,7 @@ from fcaptcha import Hcaptcha
 def solve_captcha():
     return Hcaptcha(
         site_key='4c672d35-0701-42b2-88c3-78380b0db560',
-        host='discord.com', 
+        host='discord.com/register', 
         proxy="oxp76Hrr7o6IYbaR:X3r6ouTUsWATFRd0@geo.iproyal.com:12321"
     ).solve()
 
@@ -14,13 +14,13 @@ def execute_thread():
         try:
             g = solve_captcha()
             if g:
-                httpx.post("http://192.168.1.114:9999/make_token", json={"captcha_key": g, "proxy": "oxp76Hrr7o6IYbaR:X3r6ouTUsWATFRd0@geo.iproyal.com:12321"})
+                httpx.post("http://localhost:9999/make_token", json={"captcha_key": g, "proxy": "oxp76Hrr7o6IYbaR:X3r6ouTUsWATFRd0@geo.iproyal.com:12321"})
         except Exception as e:
             print(e)
 
 if __name__ == "__main__":
     threads = []
-    for _ in range(20):
+    for _ in range(30):
         thread = threading.Thread(target=execute_thread)
         thread.start()
         threads.append(thread)
