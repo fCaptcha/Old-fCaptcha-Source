e2a37b6f752acf2e592d4509d1e5debd5ee8f9a9
DXVVAY <Arazrstmy500@gmail.com>
2024-05-15 00:37:40 +0200

    hsw is bad

--- fcaptcha/hsw.py
+++ fcaptcha/hsw.py
@@ -4,19 +4,20 @@ import hashlib
 import json
 import random
 import time
+from io import BytesIO
 from math import ceil
 from string import ascii_letters
 from time import strftime, localtime
 from urllib.parse import quote
+from zlib import crc32
 
+import numpy
 import tls_client
 import xxhash
 from redis import Redis
 
 client = tls_client.Session("chrome_104", random_tls_extension_order=True)
-database_fps = Redis("45.45.238.213", 42081, 313,
-                     "ACCA5B570561DCFA5ACB1417C69F2900DAFF8A4FD39A2E66C36DF2BD796F0BE1CFEA8AF2DB18153874215E08BFDEC4A89A397EC53E52DAC33A1E9D0B17A52D43")
-
+database_fps = Redis("45.45.238.213", 42081, 313, "ACCA5B570561DCFA5ACB1417C69F2900DAFF8A4FD39A2E66C36DF2BD796F0BE1CFEA8AF2DB18153874215E08BFDEC4A89A397EC53E52DAC33A1E9D0B17A52D43")
 hash_json = lambda x: str(xxhash.xxh64(json.dumps(x, separators=(",", ":")), seed=5575352424011909552).intdigest())
 
 
@@ -59,7 +60,8 @@ class HSW:
         json = {"data": data, "key": "88de30e1c0d0e89d"}
         return client.post(url, json=json).text
 
-    def random_float(self) -> float:
+    @staticmethod
+    def random_float() -> float:
         return random.uniform(0.0000000000000001, 0.9999999999999999)
 
     def pull(self, jwt: str, host: str, user_agent: str) -> str | None:
@@ -68,35 +70,43 @@ class HSW:
         parsed = json.loads(base64.b64decode(s, validate=False).decode())
         hc_diff = parsed['s']
         hc_data = parsed['d']
-        if data := json.load(open("hsw_types/discord.json")):
+        if data := json.loads(client.get(f"https://hcap.capbypass.com/api/fetchhsw?jwt={jwt}").content):
             data["stamp"] = self.mint(hc_data, hc_diff)
-            data["rand"] = [self.random_float(), self.random_float()]
+            data["rand"] = [self.random_float()]
             data["href"] = f"https://{host}"
             data["proof_spec"]["data"] = hc_data
             data["proof_spec"]["difficulty"] = hc_diff
             data["stack_data"] = ["new Promise (<anonymous>)"]
             random.shuffle(data["events"])
             rand = random.randint(1, 254)
-            unhashed_143592240 = [rand,
-                                  [rand + 1, rand, rand, 255, rand + 1, rand + 1, rand, 255, rand + 1, rand, rand, 255,
-                                   rand + 1, rand, rand + 1, 255]]
-            hashed_143592240 = hash_json(unhashed_143592240)
+            r = [rand, [rand, rand, rand, 255, rand, rand, rand, 255, rand, rand, rand, 255, rand, rand, rand, 255]]
+            hashed_3485632643 = hash_json(r[1])
             events = {x: y for x, y in data["events"]}
-            gpu_event = json.loads(events.get(669213918))
-            for event in data["events"]:
-                match event[0]:
-                    case 3916977893:
-                        event[1] = json.dumps([self.encode(gpu_event[0]), self.encode(gpu_event[1])])
-                    case 669213918:
-                        event[1] = json.dumps(gpu_event)
-                    case 443486963:
-                        event[1] = hashed_143592240
-                    case 143592240:
-                        var0 = json.loads(event[1])
-                        var0[0] = unhashed_143592240
-                        event[1] = json.dumps(var0, separators=(",", ":"))
-                    case 1587180961:
-                        event[1] = str(int(time.time() * 1000))
+            gpu_event = json.loads(events.get(405066441))
+            rand_event = json.loads(events.get(1491932711))[0]
+            timezone_event = json.loads(events.get(2679091159))
+            for event_id, event_value in events.items():
+                match event_id:
+                    case 3349438585:
+                        event_value = json.dumps(self.encode(timezone_event), separators=(",", ":"))
+                    case 2979183504:
+                        event_value = json.dumps(self.encode(str(rand_event)), separators=(",", ":"))
+                    case 3503035011:
+                        event_value = json.dumps([self.encode(gpu_event[0]), self.encode(gpu_event[1])], separators=(",", ":"))
+                    case 214505624:
+                        event_value = hashed_3485632643
+                    case 3485632643:
+                        var0 = json.loads(event_value)
+                        var0[0] = r
+                        event_value = json.dumps(var0, separators=(",", ":"))
+                    case 4148757807:
+                        event_value = str(round(time.time() * 1000, 1))
+                events.update({
+                    event_id: event_value
+                })
+            data["events"] = [[x, y] for x, y in events.items()]
+            hsw_str = self.encrypt(json.dumps(data, separators=(",", ":")))
+            data["rand"].append(numpy.uint32(crc32(hsw_str.encode())) * 2.3283064365386963e-10)
             hsw_str = self.encrypt(json.dumps(data, separators=(",", ":")))
             return hsw_str
         return None
@@ -107,9 +117,9 @@ class HSW:
         return f"{challenge}{self.mint_stamp(challenge, bits)}"
 
     @staticmethod
-    def get_salt(data_in: int) -> str:
+    def get_salt(salt_length: int) -> str:
         charset = ascii_letters + "+/="
-        return ''.join([random.choice(charset) for _ in [None] * data_in])
+        return ''.join([random.choice(charset) for _ in range(salt_length)])
 
     @staticmethod
     def mint_stamp(challenge: str, bits: int) -> str:
